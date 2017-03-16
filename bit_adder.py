c3dfc6abff9ca23aa6e105cc5a781a52652916bf
kevinlmadison <coolklm121@gmail.com>
2017-03-16 17:51:59 -0400

    not sure if want

---
+++ bit_adder.py
@@ -0,0 +1,12 @@
+def adder(a, b, cin): 
+
+    if cin == 0:
+        if a == b:
+            if a == 0:
+                cout = 0
+                s = 0
+            else:
+                cout = 1
+                s = 0
+
+    return cout, s
